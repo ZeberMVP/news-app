1307514641cd11c5c81a15c7980f1932fbc6e4aa
ZeberMVP <rubeen722@gmail.com>
2023-02-25 17:37:24 +0100

    add/footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,19 @@ import React, { Component } from "react";
 
 class Footer extends Component {
   render() {
-    return <div>Footer</div>;
+    return <footer>
+      <div>
+        <a href="https://www.linkedin.com/in/rub%C3%A9n-zafra-6b93151b7/" target="_blank" rel="noreferrer"><i
+          class="fa fa-linkedin" id="linkedin-icon"></i></a>
+      </div>
+      <div>
+        <a href="https://github.com/ZeberMVP" target="_blank" rel="noreferrer"><i class="fa fa-github" id="github-icon"></i></a>
+      </div>
+      <div>
+        <a href="https://www.instagram.com/zebermvp/" target="_blank" rel="noreferrer"><i class="fa fa-instagram"
+          id="instagram-icon"></i></a>
+      </div>
+    </footer>
   }
 }
 
