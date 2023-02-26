4fbcc48ebb15769ae9ac9588c6ab21c67cf1beff
ZeberMVP <rubeen722@gmail.com>
2023-02-26 17:43:54 +0100

    add/color variables

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/styles.scss'
 import App from './App';
-import '../src/styles/styles.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
